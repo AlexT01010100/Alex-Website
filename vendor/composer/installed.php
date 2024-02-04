b3d741d4c17c37d07bcc60fb671275bd1991ae90
Alexander Terry <alterry@buffalo.edu>
2024-02-04 18:24:37 -0500

    created tab bar image

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '17169/personal_website',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'da1508d39cff509ea2820cff54bc6fb9aaa6ec1c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '17169/personal_website' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'da1508d39cff509ea2820cff54bc6fb9aaa6ec1c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.9.1',
+            'version' => '6.9.1.0',
+            'reference' => '039de174cd9c17a8389754d3b877a2ed22743e18',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
